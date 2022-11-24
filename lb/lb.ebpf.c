4f2d3ff7d011b4729e0022ee46e45a1e77b521f6
asayah <adam@solo.io>
2022-11-23 20:05:30 -0700

    test

---
+++ lb/lb.ebpf.c
@@ -0,0 +1,101 @@
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+
+
+#define IP_ADDRESS(x) (unsigned int)(172 + (17 << 8) + (0 << 16) + (x << 24))
+
+#define BACKEND_A 2
+#define BACKEND_B 3
+#define CLIENT 4
+#define LB 5
+
+
+static __always_inline __u16
+csum_fold_helper(__u64 csum)
+{
+    int i;
+#pragma unroll
+    for (i = 0; i < 4; i++)
+    {
+        if (csum >> 16)
+            csum = (csum & 0xffff) + (csum >> 16);
+    }
+    return ~csum;
+}
+
+static __always_inline __u16
+iph_csum(struct iphdr *iph)
+{
+    iph->check = 0;
+    unsigned long long csum = bpf_csum_diff(0, 0, (unsigned int *)iph, sizeof(struct iphdr), 0);
+    return csum_fold_helper(csum);
+}
+
+
+#define TASK_COMM_LEN 16
+
+struct data_t {
+        __u32 fpid;
+        __u32 tpid;
+        __u64 pages;
+        char fcomm[TASK_COMM_LEN];
+        char tcomm[TASK_COMM_LEN];
+};
+
+struct {
+        __uint(type, BPF_MAP_TYPE_RINGBUF);
+        __uint(max_entries, 1 << 24);
+        __type(value, struct data_t);
+} oomkills SEC(".maps.print");
+
+
+
+SEC("xdp_lb")
+int xdp_load_balancer(struct xdp_md *ctx)
+{
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
+
+    bpf_printk("got something");
+
+    struct ethhdr *eth = data;
+    if (data + sizeof(struct ethhdr) > data_end)
+        return XDP_ABORTED;
+
+    if (bpf_ntohs(eth->h_proto) != ETH_P_IP)
+        return XDP_PASS;
+
+    struct iphdr *iph = data + sizeof(struct ethhdr);
+    if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) > data_end)
+        return XDP_ABORTED;
+
+    if (iph->protocol != IPPROTO_TCP)
+        return XDP_PASS;
+
+    bpf_printk("Got TCP packet from %x", iph->saddr);
+
+    if (iph->saddr == IP_ADDRESS(CLIENT))
+    {
+        char be = BACKEND_A;
+        if (bpf_ktime_get_ns() % 2)
+            be = BACKEND_B;
+
+        iph->daddr = IP_ADDRESS(be);
+        eth->h_dest[5] = be;
+    }
+    else
+    {
+        iph->daddr = IP_ADDRESS(CLIENT);
+        eth->h_dest[5] = CLIENT;
+    }
+    iph->saddr = IP_ADDRESS(LB);
+    eth->h_source[5] = LB;
+
+    iph->check = iph_csum(iph);
+
+    return XDP_TX;
+}
+
+char LICENSE[] SEC("license") = "GPL";
